d02516942776405ffc0a858c25f47bd18c276917
Michael Champlin <mchamplin@sbcglobal.net>
2022-05-04 19:06:06 -0700

    Code along submit

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import dataReducer from './features/dataSlice'
+import modeReducer from './features/modeSlice'
 
 export const store = configureStore({
     reducer: {
-        data: dataReducer
+        mode: modeReducer
     }
 })
